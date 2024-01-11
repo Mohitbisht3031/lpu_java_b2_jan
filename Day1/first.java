e4fc6821188d3f43f9f95605067afaa266a72dd0
unknown <imohiitsingh@gmail.com>
2024-01-11 21:00:20 +0530

    Day1

---
+++ Day1/first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class first{
+    public static void main(String[] args) {
+        // int a;
+        // Scanner sc = new Scanner(System.in);
+        // // cin>>a; this is how we do in cpp
+        // a = sc.nextInt();
+        // sc.close();
+        System.out.println(args[0] +" "+ args[1]+" "+ args[2]);
+        // System.out.println("Hello  , welcome to the java class " + a);
+    }
+}
