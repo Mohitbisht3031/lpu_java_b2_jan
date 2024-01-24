c190b63d233dea1a21ad089bda266a660a6d0530
unknown <imohiitsingh@gmail.com>
2024-01-24 21:02:17 +0530

    Day5

---
+++ Day5/loop.java
@@ -0,0 +1,19 @@
+package Day5;
+
+public class loop {
+    public static void main(String[] args) {
+
+        while(/*condition*/){/*logic nd incre/decre*/}
+
+        do{
+            /*logic && incre/decre */
+        }while(/*condition*/)
+
+        for(/*intializtion */;/*condition */ ; /*incre/decre*/){
+            /*logic */
+        }
+
+        //for each
+        for(/*element*/ : /*container*/){/*logic */}
+    }
+}
