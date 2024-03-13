100ec894be8c503f2c3fd891c447e63c10898a6f
unknown <imohiitsingh@gmail.com>
2024-03-13 20:56:04 +0530

    Day21

---
+++ Day22/example.java
@@ -0,0 +1,16 @@
+package Day22;
+
+import Day5.loop;
+
+class Person{
+    public String name;
+    public int age;
+}
+
+public class example {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.name = "mohit";
+        p.age = 13;
+    }
+}
